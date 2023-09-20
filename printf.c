49f8eec30cc942d5a280dd2ab2f381a1caeaf421
MutaiT <mutaitimo56@gmail.com>
2023-09-20 14:50:15 -0700

    Handles d and i

--- printf.c
+++ printf.c
@@ -41,9 +41,8 @@ void print_str(const char *str, int *char_count)
 int _printf(const char *format, ...)
 {
 	va_list args;
-	
-	int x = 0;
-	int character_count = 0;
+	int x = 0, character_count = 0;
+
 	va_start(args, format);
 
 	while (format[x] != '\0')
