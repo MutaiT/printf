c83eafcb65379f26cfa5c0a5cc7ff93a13ada4be
MutaiT <mutaitimo56@gmail.com>
2023-09-21 10:42:10 -0700

    Handles c,s and %

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+int main(void);
 int _printf(const char *format, ...);
 void print_str(const char *str, int *char_count);
 void print_char(char c, int *char_count);
